e78c8b232d0ef18485da80912db40c980638523a
Namgel <namgeltamang14@gmail.com>
2024-05-19 10:49:34 +0545

    Seed admin user

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import prisma from "./client";
+import bcrypt from "bcryptjs";
+
+async function main() {
+	await prisma.user.create({
+		data: {
+			name: "admin",
+			email: "gritnp@gmail.com",
+			password: bcrypt.hashSync("admin", 10),
+			role: "Admin",
+		},
+	});
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
