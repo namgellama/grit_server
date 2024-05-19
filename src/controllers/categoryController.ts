31672e9ada1828cfbc12603edc1b4877d598a3b5
Namgel <namgeltamang14@gmail.com>
2024-05-19 13:12:45 +0545

    Create PUT and GET by id APIs for category

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -13,6 +13,22 @@ const getCategories = asyncHandler(
 	}
 );
 
+// @desc Get a single category
+// @route GET /api/categories/:id
+// @access Public
+const getCategory = asyncHandler(
+	async (request: Request, response: Response) => {
+		const category = await prisma.category.findUnique({
+			where: { id: String(request.params.id) },
+		});
+
+		if (!category)
+			return response.status(404).send("Category with that Id not found");
+
+		response.send(category);
+	}
+);
+
 // @desc Create a category
 // @route POST /api/categories
 // @access Private/Admin
@@ -30,4 +46,34 @@ const createCategory = asyncHandler(
 	}
 );
 
-export { getCategories, createCategory };
+// @desc Update a category
+// @route PUT /api/categories/:id
+// @access Private/Admin
+const updateCategory = asyncHandler(
+	async (request: Request, response: Response) => {
+		const { name, image } = request.body;
+
+		const categoryExist = await prisma.category.findUnique({
+			where: { id: request.params.id },
+		});
+
+		if (!categoryExist)
+			return response
+				.status(404)
+				.send("Category with that Id not found.");
+
+		const updatedCategory = await prisma.category.update({
+			where: {
+				id: request.params.id,
+			},
+			data: {
+				name,
+				image,
+			},
+		});
+
+		response.json(updatedCategory);
+	}
+);
+
+export { getCategories, getCategory, createCategory, updateCategory };
--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -2,10 +2,13 @@ import express from "express";
 import {
 	createCategory,
 	getCategories,
+	getCategory,
+	updateCategory,
 } from "../controllers/categoryController";
 
 const router = express.Router();
 
 router.route("/").get(getCategories).post(createCategory);
+router.route("/:id").get(getCategory).put(updateCategory);
 
 export default router;
